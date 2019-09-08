dd14e51367d4bb35a22ba24f8297dde359acaa06
Matt Brigida <mdbrigida@gmail.com>
2019-09-07 22:29:59 -0400

    added risk value to app and formatting

--- src/app1.js
+++ src/app1.js
@@ -6,4 +6,17 @@ function myJsFunction()
     var Value = CFs / rate;
     document.getElementById("out1").innerHTML = Value.toFixed(2);
     document.getElementById("out2").innerHTML = CFs.toFixed(2);
+    // document.getElementById("out0").innerHTML = y1.toFixed(2);
+
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+    // document.getElementById("out0").innerHTML = this.toFixed(2);
+
+} 
